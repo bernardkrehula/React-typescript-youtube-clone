46a460958517f6b3be28b27dcd485fc7b72025c4
Bernard Krehula <krehula.bernard456@gmail.com>
2025-10-02 19:19:16 +0200

    define btn component types

--- my-app/src/components/Btn/Btn.tsx
+++ my-app/src/components/Btn/Btn.tsx
@@ -1,8 +1,12 @@
 import './Btn.css'
 
+type BtnType = {
+    onClick: () => void;
+    variation: string;
+    children: string;
+}
 
-
-const Btn = ({onClick, variation, children}) => {
+const Btn = ({onClick, variation, children}: BtnType) => {
 
     return(
         <button onClick={onClick} className={`btn ${variation}`}>{children}</button>
