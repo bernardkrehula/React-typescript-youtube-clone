0caa0eef9152e00e7e31e5549ac72b5c098e4a72
Bernard Krehula <krehula.bernard456@gmail.com>
2025-10-03 18:10:38 +0200

    menu styled

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './layout/Header/Header';
+import Menu from './layout/Menu/Menu';
 
 function App() {
 
   return (
     <div className='main'>
-      <Header></Header>
+      <Header />
+      <Menu />
     </div>
   )
 }
--- my-app/src/layout/Header/Header.tsx
+++ my-app/src/layout/Header/Header.tsx
@@ -21,7 +21,9 @@ const Header = () => {
                     <svg  xmlns="http://www.w3.org/2000/svg"  strokeWidth="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="#ffffff"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg>
                 </Btn>
             </div>
-            <img className='logo' src='./images/user-logo.jpg'></img>
+            <Btn>
+                <img className='logo' src='./images/user-logo.jpg'></img>
+            </Btn>
         </div>
     )
 }
