713bf1e3fb1386b628217249e0d04b11f0b3c207
Bernard Krehula <krehula.bernard456@gmail.com>
2025-10-10 19:29:13 +0200

    setup channel icon

--- my-app/src/components/Video/Video.tsx
+++ my-app/src/components/Video/Video.tsx
@@ -17,7 +17,7 @@ const Video = ({videoData, onClick}: VideoType) => {
     const [ timeOfPublishing , setTimeOfPublishing ] = useState('');
     const { snippet, id } = videoData;
     const { kind } = id;
-    const { channelTitle, description, publishedAt, thumbnails } = snippet;
+    const { channelTitle, title, publishedAt, thumbnails, description } = snippet;
     const { high } = thumbnails;
     console.log(thumbnails)
 
@@ -43,12 +43,21 @@ const Video = ({videoData, onClick}: VideoType) => {
             setTimeOfPublishing(`${result} year ago`);
         }
     }
-    console.log(kind)
-    if(kind != "youtube#channel") return(
+
+    if(kind === "youtube#channel") return(
+        <div className='channel-icon'>
+            <img src={high.url}/>
+            <div className='channel-icon-content'>
+                <h2>{title}</h2>
+                <h3>{description}</h3>
+            </div>
+        </div>
+    )
+    else return(
         <div className='video' onClick={onClick}>
             <img src={high.url}/> 
             <div className='video-content'>
-                <h1>{description}</h1>
+                <h1>{title}</h1>
                 <h2>{channelTitle}</h2>
                 <h3>{timeOfPublishing}</h3>
             </div>
